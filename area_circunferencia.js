9fb14914e32257eaced59d62f659ee1442d95b10
dudaborges <duda.pborges92@gmail.com>
2022-09-02 22:21:50 -0300

    addJs

---
+++ area_circunferencia.js
@@ -0,0 +1,8 @@
+//Fórmula: PI * raio * raio
+
+//normalmente variáveis constantes são escritas com letras maiúsculas
+
+const PI = 3.141592
+let raio = 10
+
+console.log(`A circunferencia com raio ${10}, tem a área de ${PI * raio * raio}`)
