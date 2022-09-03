fdacb67f7445dc62de1fecf63d9c9af58e6bc9e4
dudaborges <duda.pborges92@gmail.com>
2022-09-03 09:29:31 -0300

    addJs

--- inicio/dados.js
+++ inicio/dados.js
@@ -1,5 +1,17 @@
-console.log('caneta')
-console.log(10) //valor literal, um número difinido sem um nome.
-console.log(6.4)
-console.log(1.5)
-console.log(7.9)
+var produto = "caneta"
+var qnt = 10
+var preco = 6.4
+var imposto = 1.5
+var desconto = preco + imposto
+
+// console.log('caneta')
+// console.log(10) //valor literal, um número difinido sem um nome.
+// console.log(6.4)
+// console.log(1.5)
+// console.log(7.9)
+
+console.log(produto)
+console.log(qnt)
+console.log(preco)
+console.log(imposto)
+console.log(desconto)
