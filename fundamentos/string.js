abae135068340fad4586d6268d52667c940c9a2b
dudaborges <duda.pborges92@gmail.com>
2022-09-21 19:20:19 -0300

    update

--- fundamentos/string.js
+++ fundamentos/string.js
@@ -2,4 +2,9 @@ const escola = "Cod3r"
 
 console.log(escola.charAt(3)) //retorna a letra pelo índice
 console.log(escola.indexOf('C'))
-console.log(escola.substring(1))//retorna o resto após índice definido
+console.log(escola.substring(1))//retorna o resto após índice definido, incluindo ele mesmo
+console.log(escola.substring(2, 4))//vai do 2 ao 4)
+
+console.log('Escola '.concat(escola).concat('!'))//Concatenação
+
+console.log('Ana,Maria,João'.split(','))//para retornar um array com cada vírgula
