1935eed42aa31860833a29e26abb05af9c19d3c3
dudaborges <duda.pborges92@gmail.com>
2022-09-21 19:14:31 -0300

    update

--- fundamentos/string.js
+++ fundamentos/string.js
@@ -1,3 +1,5 @@
 const escola = "Cod3r"
 
 console.log(escola.charAt(3)) //retorna a letra pelo índice
+console.log(escola.indexOf('C'))
+console.log(escola.substring(1))//retorna o resto após índice definido
