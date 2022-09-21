d6f7b62a04d99688b1e086f100428fb084f60c34
dudaborges <duda.pborges92@gmail.com>
2022-09-21 19:43:51 -0300

    update

--- fundamentos/booleanos.js
+++ fundamentos/booleanos.js
@@ -1,3 +1,15 @@
 let isAtivo = 1
 console.log(!isAtivo) //! significa negação. 
-//Quando não há contexto de true ou false,sempre é true.
+//Quando não há contexto de true ou false, os números inteiros sempre são true, com exceção do 0
+
+console.log(!!isAtivo) //passa a ser verdadeiro, pois são dois nãos
+
+//situação que são true automaticamente
+
+console.log(!!3)
+console.log(!!-1)
+console.log(!!'')
+console.log(!!'a')
+console.log(!![])
+console.log(!!{})
+console.log(!!(isAtivo = true))
