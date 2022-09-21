5d560b905258fa45f6eb5dd283ab35807cf7e06a
dudaborges <duda.pborges92@gmail.com>
2022-09-21 18:58:30 -0300

    update

---
+++ fundamentos/cuidados-number.js
@@ -0,0 +1,6 @@
+console.log(7 / 0) //gera infinity
+console.log("10" / 5) //transforma automaticamente a string em number, por conta da tipagem fraca do js.
+
+// console.log(10.toFixed(2))
+
+console.log((10.756).toFixed(2)) //numberos literais precisam estar dentro de parênteses para chamar função
