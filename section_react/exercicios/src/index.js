972727a9bf9047f00ac3706ba27ab042b106f0fd
Maria Eduarda Borges <duda.pborges92@gmail.com>
2023-01-19 15:13:40 -0300

    react render

---
+++ section_react/exercicios/src/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+// importando as funcionalidades do arquivo packpage.json
+// tudo o que for jsx precisa ser importado o react
+// para usar o dom é necessário importar o react dom
+
+// para renderizar o elemento
+// o root é o id da div principal do arquivo index.html
+// essa sintaxe de código de colocar html dentro do js não é javascript puro, e sim uma sintaxe do react chamada jsx. É como se fosse o javascript estendido. 
+ReactDOM.render(<h1>Meu primeiro React</h1>, document.getElementById('root'))
+// esse h1 parece html, mas na verdade é jsx que converte para js nativo/puro
+
+
+const paragrafo = <p>Olá, mundo em React!</p>
+
+ReactDOM.render(paragrafo, document.getElementById('root'))
